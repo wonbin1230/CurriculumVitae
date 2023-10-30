cd8934aaaa38fc9e9f7962813d0562ac671bcfb8
OGC <wonbin12300@gmail.com>
2023-10-30 16:23:08 +0800

    feat: server port
    1.add enviroment server port

--- src/services/axiosService.ts
+++ src/services/axiosService.ts
@@ -3,7 +3,7 @@ import type { IApiSendResult } from "../modals/youtubeModal";
 
 import axios from "axios";
 
-const baseURL: string = process.env.youtubeURL || "http://127.0.0.1:5000/crawler/youtube/";
+const baseURL: string = process.env.youtubeURL || "http://192.168.1.21:5000/crawler/youtube/";
 const youtubeRequest: AxiosInstance = axios.create({ baseURL });
 
 export const apiGenPreviewVideo = (data: { url: string }) => youtubeRequest.post("/preview", data);
