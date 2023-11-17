e2166cc86e6a8caa12c4942cd481ebf148442b60
OGC <wonbin12300@gmail.com>
2023-11-17 17:12:02 +0800

    feature: remove websocket path option

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -3,4 +3,4 @@ import type { Socket } from "socket.io-client";
 import { io } from "socket.io-client";
 
 const URL: string = process.env.REACT_APP_SOCKET_URL || "http://localhost:5000";
-export const socket: Socket = io(URL, { transports: ["websocket"], path: "/websocket" });
+export const socket: Socket = io(URL, { transports: ["websocket"] });
